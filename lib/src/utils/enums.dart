848bf260f621ccb9384ccbdfa830bf515fbeadbd
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2023-11-30 20:17:54 +0530

    Removed dead code.

--- lib/src/utils/enums.dart
+++ lib/src/utils/enums.dart
@@ -19,38 +19,3 @@ enum ParenType {
   int get startRune => startStr.runes.first;
   int get endRune => endStr.runes.first;
 }
-
-enum Operator {
-  add,
-  sub,
-  mul,
-  implMul,
-  div;
-
-  static List<String> _validOperators = [
-    '+',
-    '-',
-    '*',
-    '',
-    '/',
-  ];
-
-  factory Operator.fromString(String str) {
-    switch (str) {
-      case '+':
-        return Operator.add;
-      case '-':
-        return Operator.sub;
-      case '*':
-        return Operator.mul;
-      case '/':
-        return Operator.div;
-      case '':
-        return Operator.implMul;
-      default:
-        throw UnimplementedError('Operator $str has not been implemented yet.');
-    }
-  }
-
-  static bool isValid(String str) => Operator._validOperators.contains(str);
-}
